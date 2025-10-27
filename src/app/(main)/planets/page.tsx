ad06bf4c4986a42b5f33ef5a8c3d4c53aa255b1b
Mohammad <dadsetanpor@gmail.com>
2025-10-28 00:37:51 +0330

    style(planets): change style and container size

--- src/app/(main)/planets/page.tsx
+++ src/app/(main)/planets/page.tsx
@@ -14,7 +14,7 @@ const locations: Location[] = res.data.results;
 export default function Page() {
   return (
     <main className="w-full h-full p-5">
-      <div className=" w-full h-full px-[10%]  grid grid-cols-5 grid-rows-3 items-center justify-center grid-flow-col gap-5  ">
+      <div className=" w-full h-full  grid grid-cols-5 grid-rows-3 items-center justify-center grid-flow-col gap-5  overflow-hidden  p-10">
         {PlanetsImgData.map((img, index) => {
           const justifyClass =
             index % 2 === 0
