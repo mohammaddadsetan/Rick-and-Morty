288177456251c9fc54e074d2d18f15784ac8b2b7
Mohammad <dadsetanpor@gmail.com>
2025-11-16 19:25:23 +0330

    fix(home): resolve animation issue on loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,26 @@
 "use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
-export default function page() {
+export default function Page() {
+  const [animateClass, setAnimateClass] = useState("");
+
+  useEffect(() => {
+    setAnimateClass("animate-zoom-in");
+  }, []);
+
   return (
-    <div className=" w-full flex justify-center p-2">
+    <div className="w-full flex justify-center p-2">
       <Image
         width={418}
         height={380}
-        src={"/svg/rick-car.svg"}
+        src="/svg/rick-car.svg"
         alt="car"
-        className="h-full animate-zoom-in shadow-lime-50"
-        onAnimationEnd={(e) => {
-          e.currentTarget.classList.remove("animate-zoom-in");
-          e.currentTarget.classList.add("animate-shake");
+        className={`h-full shadow-lime-50 ${animateClass}`}
+        onAnimationEnd={() => {
+          if (animateClass === "animate-zoom-in") {
+            setAnimateClass("animate-shake");
+          }
         }}
       />
     </div>
