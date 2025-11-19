defea6f55fb4aa9b4de1062216eb96adfc9db826
Mohammad <dadsetanpor@gmail.com>
2025-11-19 20:24:59 +0330

    feat(not-found): implement character and planet not found

--- src/app/(main)/characters/[character]/not-found.tsx
+++ src/app/(main)/characters/[character]/not-found.tsx
@@ -1,6 +1,35 @@
 "use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function NotFound() {
-  return <div>Character not found</div>;
+  const router = useRouter();
+  return (
+    <section className="flex items-center justify-center h-full py-10 font-mono">
+      <div className="flex items-center justify-evenly flex-col h-full ">
+        <p className="text-7xl drop-shadow-[0px_0px_5px] drop-shadow-primary-100 font-bold">
+          character not found
+        </p>
+        <p className="text-4xl max-w-150 font-bold">
+          Rick says this URL is wrong. Morty says try another one!
+        </p>
+        <div className="flex items-center justify-center flex-col w-full">
+          <Image
+            alt="image"
+            src={"/svg/rick and morthy_fuck_finger.svg"}
+            width={220}
+            height={101}
+            className="-mb-6"
+          />
+          <button
+            onClick={() => router.back()}
+            className="rounded-[50px] border-4 border-primary-100 text-5xl flex items-center justify-center w-full h-28 max-w-[300px] cursor-pointer hover:bg-primary-100 hover:text-black duration-100 transition-all">
+            Go back
+          </button>
+        </div>
+      </div>
+      <Image width={500} height={500} alt="image" src={"/svg/rick-head.svg"} />
+    </section>
+  );
 }
--- src/app/(main)/planets/[planet]/not-found.tsx
+++ src/app/(main)/planets/[planet]/not-found.tsx
@@ -1,6 +1,40 @@
 "use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function NotFound() {
-  return <div>planet not found</div>;
+  const router = useRouter();
+  return (
+    <section className="flex items-center justify-center h-full py-10 font-mono gap-20">
+      <div className="flex items-center justify-evenly flex-col h-full ">
+        <p className="text-7xl drop-shadow-[0px_0px_5px] drop-shadow-primary-100 font-bold">
+          planet not found
+        </p>
+        <p className="text-4xl max-w-150 font-bold">
+          Rick says this URL is wrong. Morty says try another one!
+        </p>
+        <div className="flex items-center justify-center flex-col w-full">
+          <Image
+            alt="image"
+            src={"/svg/rick and morthy_fuck_finger.svg"}
+            width={220}
+            height={101}
+            className="-mb-6"
+          />
+          <button
+            onClick={() => router.back()}
+            className="rounded-[50px] border-4 border-primary-100 text-5xl flex items-center justify-center w-full h-28 max-w-[300px] cursor-pointer hover:bg-primary-100 hover:text-black duration-100 transition-all">
+            Go back
+          </button>
+        </div>
+      </div>
+      <Image
+        width={200}
+        height={200}
+        alt="image"
+        src={"/svg/MORTY TALKING.svg"}
+      />
+    </section>
+  );
 }
