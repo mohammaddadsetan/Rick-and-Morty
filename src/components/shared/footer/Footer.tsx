736b9d6ea81196fc2931d543332701b3da5abb9d
Mohammad <dadsetanpor@gmail.com>
2025-12-09 17:36:16 +0330

    refactor(footer): improve responsibility

--- src/components/shared/footer/Footer.tsx
+++ src/components/shared/footer/Footer.tsx
@@ -1,46 +1,31 @@
+import MultiNavButton from "@/components/ui/MultiNavButton";
 import Image from "next/image";
 import Link from "next/link";
-
+import { HeaderLinks } from "../header/Data";
 export default function Footer() {
   return (
-    <footer>
-      <div className="px-5 py-2 bg-[#272727d2] w-full h-full flex items-center gap-9  text-neutral-50">
-        <Image
-          width={250}
-          height={90}
-          alt="logo"
-          src={"/svg/rick-and-morty-logo.svg"}
-        />
+    <footer className="px-5 py-2 bg-[#272727d2] w-full  h-fit flex items-center gap-9  text-neutral-50 font-mono  ">
+      <Link
+        href={"/"}
+        className="relative size-20 shrink-0 order-1 max-xsm:hidden">
+        <Image fill alt="logo" src={"svg/rickandmorty-logo.svg"} />
+      </Link>
 
-        <div className="w-[1px] bg-neutral-50 h-full"></div>
-        <div className="flex items-center justify-between w-full">
-          <div className="flex items-center gap-10">
-            <Link className="hover:text-[#87F54E]" href={"/"}>
-              Home
-            </Link>
-            <Link className="hover:text-[#87F54E]" href={"/planets"}>
-              Planets
-            </Link>
-            <Link className="hover:text-[#87F54E]" href={"/characters"}>
-              Characters
-            </Link>
-            <Link className="hover:text-[#87F54E]" href={"/favorites"}>
-              Favorites
-            </Link>
-          </div>
-          <div
-            className="flex flex-col items-center gap-1
-              ">
-            <p>Developer GitHub</p>
-            <Image
-              width={30}
-              height={30}
-              src={"/svg/git-logo.svg"}
-              alt="git svg"
-            />
-          </div>
-        </div>
-      </div>
+      <div className="w-[1px] bg-neutral-50 h-full order-2"></div>
+      <MultiNavButton
+        data={HeaderLinks}
+        activeMode={false}
+        linkClassName="hover:text-primary-100 text-sm sm:text-base font-semibold"
+        containerClassName="md:gap-0  justify-center w-full justify-start max-md:grid max-md:grid-cols-2 max-sm:grid-cols-1 gap-y-2 gap-x-8 order-3"
+      />
+      <Link
+        target="blank"
+        href={"https://github.com/mohammaddadsetan"}
+        className="flex flex-col items-center gap-1 hover:text-primary-100 text-sm cursor-pointer order-4 max-xsm:order-1
+          shrink-0">
+        <p className="hidden md:block font-semibold">Developer GitHub</p>
+        <Image width={40} height={40} src={"/svg/git-logo.svg"} alt="git svg" />
+      </Link>
     </footer>
   );
 }
