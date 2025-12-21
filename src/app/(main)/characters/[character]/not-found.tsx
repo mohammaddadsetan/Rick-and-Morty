65a218662982923aa7a7c9a64802e0f2c18819e8
Mohammad <dadsetanpor@gmail.com>
2025-12-21 23:00:52 +0330

    style(character/not-found): change style to respomsive page

--- src/app/(main)/characters/[character]/not-found.tsx
+++ src/app/(main)/characters/[character]/not-found.tsx
@@ -6,12 +6,12 @@ import React from "react";
 export default function NotFound() {
   const router = useRouter();
   return (
-    <section className="flex items-center justify-center h-full py-10 font-mono">
-      <div className="flex items-center justify-evenly flex-col h-full ">
-        <p className="text-7xl drop-shadow-[0px_0px_5px] drop-shadow-primary-100 font-bold">
+    <section className="flex items-center justify-center h-full gap-10 lg:gap-20 font-mono max-md:flex-col p-10 text-center">
+      <div className="flex items-center justify-evenly flex-col h-full gap-8">
+        <p className="text-4xl md:text-7xl drop-shadow-[0px_0px_5px] drop-shadow-primary-100 font-bold">
           character not found
         </p>
-        <p className="text-4xl max-w-150 font-bold">
+        <p className="text-2xl md:text-4xl max-w-150 font-bold">
           Rick says this URL is wrong. Morty says try another one!
         </p>
         <div className="flex items-center justify-center flex-col w-full">
@@ -24,12 +24,18 @@ export default function NotFound() {
           />
           <button
             onClick={() => router.back()}
-            className="rounded-[50px] border-4 border-primary-100 text-5xl flex items-center justify-center w-full h-28 max-w-[300px] cursor-pointer hover:bg-primary-100 hover:text-black duration-100 transition-all">
+            className="rounded-[50px] border-4 border-primary-100 text-2xl md:text-5xl flex items-center justify-center w-full h-15 md:h-28 max-w-[300px] cursor-pointer hover:bg-primary-100 hover:text-black duration-100 transition-all">
             Go back
           </button>
         </div>
       </div>
-      <Image width={500} height={500} alt="image" src={"/svg/rick-head.svg"} />
+      <Image
+        width={400}
+        height={400}
+        alt="image"
+        src={"/svg/rick-head.svg"}
+        className="max-lg:w-70 max-md:w-50"
+      />
     </section>
   );
 }
