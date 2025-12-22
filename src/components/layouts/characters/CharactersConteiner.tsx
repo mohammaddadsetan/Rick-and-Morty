9acd460bf4248b1a81551738f8854c40cc5cfe4f
Mohammad <dadsetanpor@gmail.com>
2025-12-22 19:42:14 +0330

    feat(charactersContainer): refactor character not found on filtercharacters

--- src/components/layouts/characters/CharactersConteiner.tsx
+++ src/components/layouts/characters/CharactersConteiner.tsx
@@ -26,16 +26,16 @@ export default async function CharactersContainer({
           currentPage={page}
         />
       ) : (
-        <div className="flex items-center justify-center h-full py-10 font-mono">
-          <p className="text-7xl drop-shadow-[0px_0px_5px] drop-shadow-primary-100 font-bold">
+        <div className="flex items-center justify-center h-full gap-10 lg:gap-20 font-mono max-md:flex-col p-10 text-center">
+          <p className="text-4xl md:text-7xl drop-shadow-[0px_0px_5px] drop-shadow-primary-100 font-bold">
             character not found
           </p>
-
           <Image
             width={300}
             height={300}
             alt="image"
             src={"/svg/rick-head.svg"}
+            className="max-lg:w-70 max-md:w-50"
           />
         </div>
       )}
