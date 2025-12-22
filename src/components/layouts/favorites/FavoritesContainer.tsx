007d3a62d039453d816bb15cd0fd37e2a8b4e954
Mohammad <dadsetanpor@gmail.com>
2025-12-22 19:43:01 +0330

    feat(FavoritesContainer): add favorites banner on page

--- src/components/layouts/favorites/FavoritesContainer.tsx
+++ src/components/layouts/favorites/FavoritesContainer.tsx
@@ -1,7 +1,48 @@
-import { getCharacters } from "@/services/rickandmorty";
-import Characters from "../characters/Characters";
+"use client";
 
-export default async function FavoritesContainer() {
-  const characters = await getCharacters();
-  return <Characters character={characters} />;
+import { CharacterType } from "@/services/rickandmorty";
+import CharacterCard from "../characters/CharacterCard";
+import { useContext } from "react";
+import { FavoriteContext } from "@/context/FavoriteContext";
+import { CharacterContext } from "@/context/CharacterContext/CharacterContext";
+import Image from "next/image";
+
+export default function FavoritesContainer() {
+  const ctx = useContext(FavoriteContext);
+  const context = useContext(CharacterContext);
+
+  if (!ctx || !context) return null;
+
+  const { favorites } = ctx;
+  const characters = context.characters || [];
+
+  const planetCharacters = characters.filter((char: CharacterType) =>
+    favorites?.includes(char.id)
+  );
+
+  return (
+    <div className="flex flex-col items-center justify-between gap-10 py-10 md:px-10 px-5">
+      <Image alt="image" src={"/svg/favorites.svg"} width={400} height={300} />
+      {planetCharacters.length > 0 ? (
+        <div className="flex flex-wrap justify-center items-center w-auto mx-auto my-10 gap-10">
+          {planetCharacters.map((item) => (
+            <CharacterCard
+              key={item.id}
+              character_img={item.image}
+              character_name={item.name}
+              species={item.species}
+              status={item.status}
+              id={item.id}
+            />
+          ))}
+        </div>
+      ) : (
+        <p className=" md:text-2xl drop-shadow-[0px_0px_5px] drop-shadow-primary-100 font-bold max-w-[500px] text-center">
+          There doesn't seem to be anything to show here currently, if you see a
+          character you want to add to favorites, hit the white heart so you can
+          see them here!
+        </p>
+      )}
+    </div>
+  );
 }
