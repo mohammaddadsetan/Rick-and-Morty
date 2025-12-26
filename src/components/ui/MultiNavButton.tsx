7049fafd57b72b90dfd5e8788d9aaf3b882268a6
Mohammad <dadsetanpor@gmail.com>
2025-12-26 21:52:33 +0330

    refactor(multiNavButton): add dark and light colors variation

--- src/components/ui/MultiNavButton.tsx
+++ src/components/ui/MultiNavButton.tsx
@@ -54,11 +54,11 @@ export default function MultiNavButton({
               <span className="absolute inset-0 z-0 overflow-hidden   rounded-lg ">
                 <span className="absolute  animate-border-spin   inset-[-1000%]  bg-[conic-gradient(from_0deg,transparent_0deg_80%,#ffffff_90%,transparent_100%)]" />
 
-                <span className="absolute inset-[2px]  bg-black rounded-[5px] z-10"></span>
+                <span className="absolute inset-[2px]  bg-[var(--neutral)] rounded-[5px] z-10"></span>
               </span>
             )}
 
-            <span className="relative z-20 flex  items-center justify-center gap-1">
+            <span className="relative z-20 flex  items-center justify-center gap-1 font-mono  ">
               {item.text}
               {countBox === index + 1 && (countNumber ?? 0) >= 0 && (
                 <span className=" flex items-center justify-center text-white bg-red-500 rounded-full text-sm  font-mono  size-5 shrink-0">
