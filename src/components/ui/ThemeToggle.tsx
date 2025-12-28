103c3c1bb12107e7965864cf2037f1451a4cede0
Mohammad <dadsetanpor@gmail.com>
2025-12-28 19:49:10 +0330

    feat(ThemeToggle): add new style and future to open box

--- src/components/ui/ThemeToggle.tsx
+++ src/components/ui/ThemeToggle.tsx
@@ -1,21 +1,34 @@
 "use client";
 
+import { ChevronRightIcon } from "lucide-react";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => setMounted(true), []);
   if (!mounted) return null;
 
   return (
-    <button
-      aria-label="Toggle theme"
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      className=" px-3 py-2 rounded-lg text-2xl absolute bg-neutral-800  top-1/3 z-50 -left-10 hover:left-1 transition-all duration-300 cursor-pointer">
-      {theme === "dark" ? "🌙" : "☀️"}
-    </button>
+    <div
+      className={` sm:px-2 p-1 py-2 rounded-lg text-2xl flex items-center justify-center fixed  bg-neutral-800 border-2 border-(var--neutral)  bottom-15 z-50 ${
+        isOpen ? "left-1" : " -left-13"
+      } sm:gap-2 gap-1 transition-all duration-300 cursor-pointer`}>
+      <button
+        aria-label="Toggle theme"
+        className="p-1 cursor-pointer"
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+        {theme === "dark" ? "🌙" : "☀️"}
+      </button>
+      <ChevronRightIcon
+        onClick={() => setIsOpen(!isOpen)}
+        className={` transition-all duration-200 ${
+          isOpen ? "rotate-180" : "rotate-0"
+        }`}
+      />
+    </div>
   );
 }
