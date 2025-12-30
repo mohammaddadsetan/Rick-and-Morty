cc7dd458810cf22674a01d8f62a19d522afdd174
Mohammad <dadsetanpor@gmail.com>
2025-12-30 11:50:41 +0330

    fix(not-found): resolve responsive issue

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,10 +3,12 @@ import React from "react";
 
 export default function Error() {
   return (
-    <section className="flex flex-col items-center justify-center px-20 py-10 font-mono gap-10">
-      <h3 className="text-center text-9xl">Error 404</h3>
-      <div className="flex items-start justify-between gap-10">
-        <p className="text-7xl max-w-[700px]">
+    <section className="flex flex-col items-center justify-center px-5 sm:px-10 py-10 font-mono gap-10 text-(--text)">
+      <h3 className="text-center text-6xl md:text-8xl lg:text-9xl font-bold ">
+        Error 404
+      </h3>
+      <div className="flex items-center justify-center gap-10 max-md:flex-col-reverse">
+        <p className="text-4xl md:text-6xl lg:text-7xl max-w-[700px] max-md:text-center">
           Don’t worry my friend, not an alien penis... Flip the pickle.
         </p>
         <Image
@@ -14,6 +16,7 @@ export default function Error() {
           height={190}
           alt="image"
           src={"/svg/pikle-rick-vectorize 1.svg"}
+          className="w-40 md:w-50 lg:w-60"
         />
       </div>
     </section>
